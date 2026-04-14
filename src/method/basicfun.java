6ad0a0fa8f112db3b5c4d309b9280f0d58cc2419
rushikesh_saykar <rushikeshdsaykar@gmail.com>
2026-04-14 11:44:25 +0530

    day 6

---
+++ src/method/basicfun.java
@@ -0,0 +1,12 @@
+package method;
+
+public class basicfun {
+    public static void printname(){
+        System.out.println("Rushikesh");
+
+    }
+
+    public static void main(String[] args) {
+         printname();
+    }
+}
