7448a997e8d15049388009d2ab8060c2e8178c56
rushikesh_saykar <rushikeshdsaykar@gmail.com>
2026-04-22 15:51:23 +0530

    bubble understand

--- src/basic_sorting/bubble_sort/bubbleSort_optimiz.java
+++ src/basic_sorting/bubble_sort/bubbleSort_optimiz.java
@@ -1,4 +1,30 @@
 package basic_sorting.bubble_sort;
 
+
+
 public class bubbleSort_optimiz {
+
+    public static void main(String[] args) {
+
+        int [] arr={ 1,2,6,4};
+        int n=arr.length;
+
+        for(int i=0;i<n-1;i++){
+            int swap=0;
+            for(int j=0;j<n-1-i;j++){
+                if(arr[j]>arr[j+1]){
+                    int temp=arr[j];
+                    arr[j]=arr[j+1];
+                    arr[j+1]=temp;
+                    swap++;
+                }
+            }
+            if(swap==0) break;
+
+        }
+      for(int element:arr){
+          System.out.print(element+" ");
+      }
+    }
+
 }
--- src/basic_sorting/bubble_sort/sortArray_desending.java
+++ src/basic_sorting/bubble_sort/sortArray_desending.java
@@ -2,6 +2,21 @@ package basic_sorting.bubble_sort;
 
 public class sortArray_desending {
     public static void main(String[] args) {
+         int [] arr={2,4,1,4,6,50};
+         int n=arr.length;
 
+         for(int i=0;i<n-1;i++){
+             for(int j=0;j<n-1-i;j++){
+                 if(arr[j]<arr[j+1]){
+                     int temp=arr[j];
+                     arr[j]=arr[j+1];
+                     arr[j+1]=temp;
+                 }
+             }
+
+         }
+         for(int element:arr){
+             System.out.print(element+" ");
+         }
     }
 }
